995a512a8ed0978c7f34d759234f14b3082655db
Tye Stanley <tyestanley090@gmail.com>
2024-01-25 23:09:30 -0800

    updated tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {},
+      colors: {
+        primary: '#833EFF',
+        secondary: '#7934B0',
+        ps100: '#94A7CB',
+        ps50: '#C3D4E9',
+        gray900: '#1A202C',
+        gray850: '#293346',
+        gray800: '#424B5C',
+        gray700: '#3D5278',
+        gray400: '#90A3BF',
+        white: '#FFFFFF',
+        white100: '#F7F9FC',
+        white200: '#F6F7F9',
+      },
+      screens: {
+        xs: '475px',
+        xl: '1440px',
+      },
     },
   },
   plugins: [],
