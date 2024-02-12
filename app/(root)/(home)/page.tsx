0b51e97ec4e662c28ed45006cb23bdb3949a4fe4
Tye Stanley <tyestanley090@gmail.com>
2024-02-11 19:05:49 -0800

    made car images responsive for all screen sizes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,8 +2,50 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="mt-8 max-w-2xl px-5 lg:px-10">
-      <div className="flex justify-center gap-2"></div>
+    <main className="bg-white200 px-6 py-8 dark:bg-gray900 lg:px-10">
+      <div className="mx-auto max-w-[1440px]">
+        <div className="relative flex w-full justify-center gap-5">
+          <Image
+            src="/homepage/mobile_ad1.svg"
+            alt="ads"
+            width="327"
+            height="232"
+            className="w-full lg:hidden"
+          />
+
+          <Image
+            src="/homepage/tablet_ad1.svg"
+            alt="ads"
+            width="471"
+            height="360"
+            className="hidden w-full lg:block xl:hidden"
+          />
+
+          <Image
+            src="/homepage/tablet_ad2.svg"
+            alt="ads"
+            width="471"
+            height="360"
+            className="hidden w-full lg:block xl:hidden"
+          />
+
+          <Image
+            src="/homepage/desktop_ad1.svg"
+            alt="ads"
+            width="640"
+            height="360"
+            className="hidden w-full xl:block"
+          />
+
+          <Image
+            src="/homepage/desktop_ad2.svg"
+            alt="ads"
+            width="640"
+            height="360"
+            className="hidden w-full xl:block"
+          />
+        </div>
+      </div>
     </main>
   );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import NavbarMobile from '@/components/Navbar/NavbarMobile';
 
 const Navbar = () => {
   return (
-    <nav className="w-full border-b border-[#C3D4E9]/40 bg-white px-6 py-8 dark:border-gray850 dark:bg-gray900 md:py-5">
-      <div className="flex items-center justify-between">
+    <nav className="w-full border-b border-[#C3D4E9]/40 bg-white px-6 py-8 dark:border-gray850 dark:bg-gray900 md:py-5 lg:px-10">
+      <div className="mx-auto flex max-w-[1440px] items-center justify-between">
         <NavbarLogo />
 
         <NavbarMobile />
