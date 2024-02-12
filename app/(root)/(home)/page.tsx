c95d8903e97d13c56611b8eea98786396e0d22a9
Tye Stanley <tyestanley090@gmail.com>
2024-02-11 18:28:57 -0800

    added car ad images for homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world
+    <main className="mt-8 max-w-2xl px-5 lg:px-10">
+      <div className="flex justify-center gap-2"></div>
     </main>
   );
 }
