6fab77c72bba4f59fc1b6ba7940ab87f50074f16
Tye Stanley <tyestanley090@gmail.com>
2024-02-22 18:44:35 -0800

    updated navbar to have a fixed width to align with homepage

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import NavbarMobile from '@/components/Navbar/NavbarMobile';
 
 const Navbar = () => {
   return (
-    <nav className="w-full border-b border-[#C3D4E9]/40 bg-white px-6 py-8 dark:border-gray850 dark:bg-gray900 md:py-5 xl:px-16">
-      <div className="mx-auto flex max-w-[1440px] items-center justify-between">
+    <nav className="border-b border-[#C3D4E9]/40 bg-white dark:border-gray850 dark:bg-gray900">
+      <div className="mx-auto flex items-center justify-between px-6 py-8 md:py-5 lg:max-w-[1024px] xl:max-w-[1440px] xl:px-16">
         <NavbarLogo />
 
         <NavbarMobile />
